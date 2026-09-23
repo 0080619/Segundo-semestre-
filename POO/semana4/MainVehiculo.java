edc9af1687ec5e04de1c4ed9c10049a5ceabaf64
0080619 <maria.puentess@campusucc.edu.co>
2026-09-23 21:55:01 +0000

    getup

--- POO/semana4/MainVehiculo.java
+++ POO/semana4/MainVehiculo.java
@@ -1,10 +1,12 @@
 public class MainVehiculo {
     public static void main(String[] args) {
         // creacion de objeto
-        Vehiculo objVehiculo1 = new Vehiculo("Toyota", "Corolla", 2020, 20000.0);
-        
+        Vehiculo objVehiculo1 = new Vehiculo("Mazda", "x30", 2020, 80000000.0);
+        Vehiculo objVehiculo2 = new Vehiculo("Honda", "Civic", 2019, 18000000.0);
         System.out.println(objVehiculo1.toString());
-        
+        System.out.println(objVehiculo2.toString());
+
         objVehiculo1.mostrarInformacion();
+        objVehiculo2.mostrarInformacion();
     }
 }
